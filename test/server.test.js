8b0fc1e2d03a48500f30f658857ba53344baca18
shahih25 <hmshahid@asu.edu>
2024-03-26 14:43:01 -0700

    [feature-1-setup] add a test

--- test/server.test.js
+++ test/server.test.js
@@ -1,9 +1,16 @@
 import request from "supertest";
 import app from "../server/app";
 
-describe('GET /', () => {
+describe('GET hello world', () => {
     it("should return a response", async () => {
         const res = await request(app).get("/");
         expect(res.statusCode).toBe(200);
+    })
+})
+
+describe("GET API doesn't exist", () => {
+    it("should return a bad response", async () => {
+        const res = await request(app).get("/sample");
+        expect(res.statusCode).toBe(404);
     })
 })
